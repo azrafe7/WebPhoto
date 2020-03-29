1f94d4f0be47abd46962bbc56491cca3e166f33a
azrafe7 <azrafe7@gmail.com>
2020-03-29 01:43:54 +0100

    [start server.bat] use python 3

--- start server.bat
+++ start server.bat
@@ -1,3 +1,8 @@
 @echo off
 echo Starting HTTP server for webphoto...
-c:\python27\python -m SimpleHTTPServer
+
+REM for python 2.x
+REM python -m SimpleHTTPServer
+
+REM for python 3.x
+python -m http.server
